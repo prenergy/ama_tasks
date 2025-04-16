218facae5e450a68c1cc061b14ae4f59880047c7
prenergy <prenergy222@gmail.com>
2025-04-16 19:54:52 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,20 @@
 self.addEventListener('install', function(event) {
   console.log('Service Worker Installed');
-  // کش کردن فایل‌ها
+  event.waitUntil(
+    caches.open('pwa-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/app.js',
+        '/manifest.json',
+        '/icons/icon.png'
+      ]);
+    })
+  );
 });
 
 self.addEventListener('fetch', function(event) {
-  console.log('Fetch event for ' + event.request.url);
   event.respondWith(
     caches.match(event.request)
       .then(function(response) {
